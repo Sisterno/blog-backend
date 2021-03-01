9c3ba96acab7dba42db39c96c9ae24167188cea8
Gonzalo Rojas Alvarez <sisterno1998@gmail.com>
2021-03-01 11:33:43 -0500

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2d1dbaf15f5898844026795a36ec62e0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6b138158-c033-4c56-a9e4-039f2500cf98'
+};
